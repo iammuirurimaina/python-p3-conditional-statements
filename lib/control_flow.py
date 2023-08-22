3f8c5565e510bcd7c520bb0955afd7730d94fe38
ian <ian.maina@student.moringaschool.com>
2023-08-22 14:06:46 +0300

    Assignment completed

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -1,17 +1,60 @@
 #!/usr/bin/env python3
 
 def admin_login(username, password):
-    # your code here
+    if (username == "admin" or username == "ADMIN") and password == '12345':
+        return "Access granted"
+    else:
+        return "Access denied"
+
     pass
 
+
 def hows_the_weather(temperature):
-    # your code here
+    temperature == 20
+    # below 40, return "It's brisk out there!". 
+    if temperature < 40:
+        print("It's brisk out there!") 
+        return "It's brisk out there!"
+    
+    
+    # If the temperature is between 40 and 65,
+    elif temperature > 40 and temperature < 65:
+        return "It's a little chilly out there!"
+    # If the temperature is above 85, return "It's too dang hot out there!". 
+    elif temperature > 85:
+        return "It's too dang hot out there!"
+        
+    # Otherwise, return "It's perfect out there!"
+    else:
+        return "It's perfect out there!"
+        
+
+
     pass
 
 def fizzbuzz(num):
-    # your code here
+    if num % 3 == 0 and num % 5 == 0:
+        return "FizzBuzz"
+    elif num % 3 == 0:
+        return "Fizz"
+    elif num % 5 == 0:
+        return "Buzz"
+    
+    else:
+        return num
     pass
 
 def calculator(operation, num1, num2):
-    # your code here
+    if operation == "+":
+        return num1 + num2
+    elif operation == "-":
+        return num1 - num2
+    elif operation == "*":
+        return num1 * num2
+    elif operation == "/":
+        return num1/num2
+    else:
+        print ("Invalid operation!")
+        return None
+        
     pass
